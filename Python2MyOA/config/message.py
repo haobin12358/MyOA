cc93decc6b8ae54019a27a2bec0ac453e969b9f6
shuaibinliu <13588046135@163.com>
2018-03-03 12:03:57 +0800

    message modify

--- Python2MyOA/config/message.py
+++ Python2MyOA/config/message.py
@@ -1,2 +1,7 @@
 # *- coding:utf8 *-
 NO_APIS = "the apis is not found"
+PWD_ERROR = "the user or password error. "  # 原始密码错误
+PWD_EQUAL_OLD = "the new password is same as old password. "  # 原始密码与新密码相同
+PWD_OUTER = "the password is too long. "  # 密码过长
+NO_USER = "the user is not found ."  # 用户不存在
+# 405102  # 密码错误 api 文档中描述error，和405103类似故不重复
